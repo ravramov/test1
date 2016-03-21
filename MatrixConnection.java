982d5912683668a9ef972fb1e75aac4b922197b3
R.Avramov <avram0v@mail.ru>
2016-03-21 12:02:32 +0300

    asfsdf

--- MatrixConnection.java
+++ MatrixConnection.java
@@ -1,20 +1,2 @@
-/*
- * Copyright (C) 2004 Datalex. All Rights Reserved.
- *
- * This software is the confidential and proprietary information of Datalex
- * ("Confidential Information"). You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the
- * licence agreement you entered into with Datalex.
- *
- * $Log: MatrixConnection.java,v $
- * Revision 1.4  2004/06/02 10:00:51  aharan
- * Deleted commented out old send/recieve methods as their 
- * javadoc were interferring with new send/recieve's javadoc 
- * with the result that warning was occuring during build that 
- * the new send/recieve methods were depreciated.
- *
- * Revision 1.3  2004/05/04 10:57:04  acraig
- * Removed references to obsolete send/receive methods
- *
- * Revision 1.2  2004/04/30 13:41:12  acraig
- 
+THIS IS VERY IMPORTANT!
+THIS WAS DANO IN MASTER!
